4ca13b6b838b99a3ac5ee483cb1eb96af039354c
Ankit Sangwan <ankit.sangwan1999@gmail.com>
2020-02-04 02:47:49 +0530

    Updates

---
+++ precache-manifest.0cfc3e651f0eac17e297e1496b3eb8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fcdcacf575898851a77ef27d7524eb",
+    "url": "/ankitsangwan1999/Project-INSIGHT/index.html"
+  },
+  {
+    "revision": "7f6e407d928b4bd5fdad",
+    "url": "/ankitsangwan1999/Project-INSIGHT/static/js/2.1ce3a5fb.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/ankitsangwan1999/Project-INSIGHT/static/js/2.1ce3a5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e277339c78b83858537",
+    "url": "/ankitsangwan1999/Project-INSIGHT/static/js/main.1cb4b29e.chunk.js"
+  },
+  {
+    "revision": "51ca4626613a8d2bdacc",
+    "url": "/ankitsangwan1999/Project-INSIGHT/static/js/runtime-main.c198dfff.js"
+  }
+]);
